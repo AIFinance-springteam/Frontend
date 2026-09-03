eb8664ca174e4d2777e14e27b0d285584b28db15
Lee SeungJoon <sj020521@naver.com>
2026-09-03 16:36:36 +0900

    Reapply "fix: 메인 진입 시 로그인 여부에 따라 분기"
    This reverts commit 3de21ea6134b7087933a66e62e79f74fd622e6ea.

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,9 +1,17 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { useReceiptSplit } from '../features/settlement/hooks/useReceiptSplit'
 import { tripParticipants } from '../features/trip/mocks/tripMockData'
+import { tokenStorage } from '../shared/api/tokenStorage'
 import { ReceiptSplitRoute } from './routes/ReceiptSplitRoute'
 import { routePaths } from './routes/routePaths'
 import { TripHomeRoute } from './routes/TripHomeRoute'
+import LoginPage from '../pages/auth/LoginPage'
+import SignupPage from '../pages/auth/SignupPage'
+import TripsPage from '../pages/trips/TripsPage'
+import TripMembersPage from '../pages/trips/TripMembersPage'
+import InvitedTripPage from '../pages/trips/InvitedTripPage'
+import ReceiptNewPage from '../pages/trips/receipts/ReceiptNewPage'
+import ReceiptDetailPage from '../pages/trips/receipts/ReceiptDetailPage'
 
 export function AppRouter() {
   return (
@@ -18,11 +26,21 @@ function AppRoutes() {
 
   return (
     <Routes>
-      <Route path="/" element={<Navigate to={routePaths.tripHome()} replace />} />
+      <Route
+        path="/"
+        element={<Navigate to={tokenStorage.get() ? routePaths.tripHome() : '/login'} replace />}
+      />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/trips" element={<TripsPage />} />
       <Route
         path="/trips/:tripId"
         element={<TripHomeRoute unassignedCount={receiptSplit.unassignedCount} />}
       />
+      <Route path="/trips/:tripId/members" element={<TripMembersPage />} />
+      <Route path="/trips/:tripId/receipts/new" element={<ReceiptNewPage />} />
+      <Route path="/trips/:tripId/receipts/:receiptId" element={<ReceiptDetailPage />} />
+      <Route path="/invite/:inviteCode" element={<InvitedTripPage />} />
       <Route
         path="/receipts/:receiptId/split"
         element={
